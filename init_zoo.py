d48b4ff5731bb5b5714e9d4cdf6e8855ccca3cba
orya <orya.sp@gmail.com>
2022-11-03 15:13:54 +0200

    Add more animals to init_zoo.py

--- init_zoo.py
+++ init_zoo.py
@@ -1,10 +1,19 @@
 from animals.species.lion import lion
+from animals.species.whale import whale
+from animals.species.goose import goose
+from animals.species.snowy_owl import snowy_owl
+from animals.species.clownfish import clownfish
 
 
 def init(zoo):
     Simba = lion("Simba")
     Nala = lion("Nala")
+    Willy = whale("Willy")
+    Akka = goose("Akka")
+    Hedwig = snowy_owl("Hedwig")
+    Nemo = clownfish("Nemo")
+    Marlin = clownfish("Marlin")
 
-    animals = [Simba, Nala]
+    animals = [Simba, Nala, Willy, Akka, Hedwig, Nemo, Marlin]
     zoo.add_animals(animals)
-    zoo.print_zoo()
+    # zoo.print_zoo()
