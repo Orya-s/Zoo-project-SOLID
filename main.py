1f6614f21e231d00b96b4a1395fd8ae9e149365d
orya <orya.sp@gmail.com>
2022-11-03 12:13:15 +0200

    Add main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from zoo import zoo
+from init_zoo import init
+
+# creating my zoo
+zoo = zoo()
+init(zoo)
+
+# 2 weeks simulation:
+NUM_DAYS = 14
+
+for day in range(NUM_DAYS):
+    zoo.feed_all()
