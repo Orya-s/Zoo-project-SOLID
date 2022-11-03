9979f998f745576174aa8facceb7c610752dce95
orya <orya.sp@gmail.com>
2022-11-03 15:13:25 +0200

    Edit restock_amount per species in zoo

--- zoo.py
+++ zoo.py
@@ -5,18 +5,21 @@ class zoo:
     def __init__(self):
         self.animals = []
         self.food_supply = {}   # {"food_type":amount}
-        self.restock_amount = 50
+        self.restock_amount = {}    # {"food_type":amount}
+        self.restock_mul = 5
         
     def add_animal(self, animal:animal):
         self.animals.append(animal)
-        self.add_food(animal.get_food().get_type())
+        self.add_food(animal.get_food())
         
     def add_animals(self, animals):
         [self.add_animal(a) for a in animals]
         
     def add_food(self, food:food):
-        if food not in self.food_supply:
-            self.food_supply[food] = self.restock_amount
+        food_type = food.get_type()
+        if food_type not in self.food_supply:
+            self.restock_amount[food_type] = food.get_amount() * self.restock_mul
+            self.food_supply[food_type] = self.restock_amount[food_type]
             
     def feed_all(self):
         [self.feed_animal(animal.get_food()) for animal in self.animals]    # in feed_animal- recievs food   # map ?
@@ -28,9 +31,9 @@ class zoo:
             self.restock(food_type)
         self.food_supply[food_type] -= amount
         
-    def restock(self, food):
-        print("{} ran out, filling the supply...".format(food))
-        self.food_supply[food] += self.restock_amount
+    def restock(self, food_type):
+        print("{} ran out, filling the supply...".format(food_type))
+        self.food_supply[food_type] += self.restock_amount[food_type]
         
     def print_zoo(self):    # operator overloading __str__ 
         print(self.animals)
