96ca7391e983e869b7445c819d7d2e19a8492316
orya <orya.sp@gmail.com>
2022-11-03 12:10:20 +0200

    Add animals directory

--- bird.py
+++
@@ -1,6 +0,0 @@
-from animal import animal
-
-class bird(animal):
-    def __init__(self, name, species, food, wing_span):
-        super().__init__(name, species, food)
-        self.wing_span = wing_span
--- sea_creature.py
+++
@@ -1,6 +0,0 @@
-from animal import animal
-
-class sea_creature(animal):
-    def __init__(self, name, species, food, lowest_depth):
-        super().__init__(name, species, food)
-        self.lowest_depth = lowest_depth
