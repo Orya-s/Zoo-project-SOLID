c1453baad9a02c2303bb15b602d208e9d78ea162
orya <orya.sp@gmail.com>
2022-11-03 12:08:52 +0200

    Add feeding functions to zoo.py

--- zoo.py
+++ zoo.py
@@ -1,27 +1,36 @@
+from food import food
+from animal import animal
 
 class zoo:
     def __init__(self):
         self.animals = []
         self.food_supply = {}   # {"food_type":amount}
-        self.restock_amount = 100
+        self.restock_amount = 50
         
-    def add_animal(self, animal):
+    def add_animal(self, animal:animal):
         self.animals.append(animal)
         self.add_food(animal.get_food().get_type())
         
-    def add_food(self, food):
+    def add_animals(self, animals):
+        [self.add_animal(a) for a in animals]
+        
+    def add_food(self, food:food):
         if food not in self.food_supply:
             self.food_supply[food] = self.restock_amount
             
-    def print_zoo(self):
-        print(self.animals)
+    def feed_all(self):
+        [self.feed_animal(animal.get_food()) for animal in self.animals]    # in feed_animal- recievs food   # map ?
         
-    def feed_animal(self, food_type, amount):
+    def feed_animal(self, food:food):
+        food_type = food.get_type()
+        amount = food.get_amount()
         if self.food_supply[food_type] - amount < 0:
             self.restock(food_type)
         self.food_supply[food_type] -= amount
         
     def restock(self, food):
-        print("{} ran out, filling the suplly...".format(food))
+        print("{} ran out, filling the supply...".format(food))
         self.food_supply[food] += self.restock_amount
         
+    def print_zoo(self):    # operator overloading __str__ 
+        print(self.animals)
